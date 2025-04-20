2daef10d6b672ac9b97b2cb703e510e81ea4296c
Rich Cherngchaosil <richcherng@gmail.com>
2025-04-20 02:09:07 +0000

    Setup Gradle

---
+++ Gardener/src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DemoApplicationTests {
+    @Test
+    void sampleTest() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
